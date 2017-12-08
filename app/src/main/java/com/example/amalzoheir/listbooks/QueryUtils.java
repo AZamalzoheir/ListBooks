ec94bbbca316326e2afb60c83471cb46ab624410
AZamalzoheir <A.Zamalzoheir95@gamil.com>
2017-12-08 11:58:38 +0200

    finaly commit

--- app/src/main/java/com/example/amalzoheir/listbooks/QueryUtils.java
+++ app/src/main/java/com/example/amalzoheir/listbooks/QueryUtils.java
@@ -34,8 +34,8 @@ public final class QueryUtils {
 
     public static ArrayList<ListBook> extractListBooks(String bURL) {
         ArrayList<ListBook> listBooks = new ArrayList<>();
-        ArrayList<String> bookAuthor=new ArrayList<>();
-        String sBookAuthor="";
+        ArrayList<String> bookAuthor = new ArrayList<>();
+        String sBookAuthor = "";
         try {
             JSONObject root = new JSONObject(bURL);
             JSONArray items = root.getJSONArray("items");
@@ -44,29 +44,30 @@ public final class QueryUtils {
                 JSONObject volumInfo = listBook.getJSONObject("volumeInfo");
                 JSONArray authors = volumInfo.getJSONArray("authors");
                 String bookName = volumInfo.getString("title");
-                for(int j=0;j<authors.length();j++) {
+                for (int j = 0; j < authors.length(); j++) {
                     bookAuthor.add(authors.getString(j));
                 }
-                sBookAuthor=bookAuthor.get(i);
-                listBooks.add(new ListBook(bookName,sBookAuthor));
+                sBookAuthor = bookAuthor.get(i);
+                listBooks.add(new ListBook(bookName, sBookAuthor));
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return listBooks;
     }
-    private static URL createUrl(String url) throws MalformedInputException{
-        URL bURL=null;
-        try{
-            bURL=new URL(url);
-        }
-        catch (MalformedURLException e) {
+
+    private static URL createUrl(String url) throws MalformedInputException {
+        URL bURL = null;
+        try {
+            bURL = new URL(url);
+        } catch (MalformedURLException e) {
             Log.e("inquaryutil", "error when making url", e);
             return null;
         }
         return bURL;
     }
-    public static String makeHttpRequaste(URL burl)throws IOError, IOException {
+
+    public static String makeHttpRequaste(URL burl) throws IOError, IOException {
         String jsonResponse = "";
         if (burl == null) {
             return jsonResponse;
@@ -98,25 +99,26 @@ public final class QueryUtils {
     }
 
     public static String readFromStream(InputStream inputstream) throws IOException {
-            StringBuilder output = new StringBuilder();
-            try {
-                if (inputstream != null) {
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputstream, Charset.forName("UTF-8"));
-                    BufferedReader reader = new BufferedReader(inputStreamReader);
-                    String line = reader.readLine();
-                    while (line != null) {
-                        output.append(line);
-                        line = reader.readLine();
-                    }
+        StringBuilder output = new StringBuilder();
+        try {
+            if (inputstream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputstream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
                 }
-            } catch (IOException e) {
-                Log.e("inquaryutil", "error when make read from file", e);
             }
-            return output.toString();
+        } catch (IOException e) {
+            Log.e("inquaryutil", "error when make read from file", e);
         }
-    public static ArrayList<ListBook> fetchListbookData(String requestUrl) throws MalformedInputException{
+        return output.toString();
+    }
+
+    public static ArrayList<ListBook> fetchListbookData(String requestUrl) throws MalformedInputException {
         // Create URL object
-        URL url =createUrl(requestUrl);
+        URL url = createUrl(requestUrl);
 
         // Perform HTTP request to the URL and receive a JSON response back
         String jsonResponse = null;
@@ -126,10 +128,10 @@ public final class QueryUtils {
             Log.e("query", "Problem making the HTTP request.", e);
         }
         // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
-        ArrayList<ListBook> listBooks =extractListBooks(jsonResponse);
+        ArrayList<ListBook> listBooks = extractListBooks(jsonResponse);
 
         // Return the list of {@link Earthquake}s
         return listBooks;
     }
-    }
+}
 
